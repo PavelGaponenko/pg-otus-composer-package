9c7ced4013df96ccb77645922c1d6c0c6eed0959
Павел <p.gaponenko@worksolutions.ru>
2022-02-18 22:17:25 +0300

    Реализована валидация Json строки в методе преобразования в массив

--- src/Service/JsonDecoder.php
+++ src/Service/JsonDecoder.php
@@ -6,11 +6,23 @@ class JsonDecoder
 {
     public function toArray(string $json): array
     {
+        if (!$this->validateJson($json)) {
+            return [];
+        }
+
         return json_decode($json, true);
     }
 
     public function toJson(array $array): string
     {
         return json_encode($array);
+    }
+
+    private function validateJson(string $json): bool
+    {
+        return preg_match(
+            '/[^,:{}\\[\\]0-9.\\-+Eaeflnr-u \\n\\r\\t]/',
+            preg_replace('/"(\\.|[^"\\\\])*"/', '', $json)
+        );
     }
 }
